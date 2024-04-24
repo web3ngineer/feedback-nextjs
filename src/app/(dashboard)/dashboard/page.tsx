be81bc92cc9acf8dfae7137ecf129a3d020b402b
Shvam Singh <singhshivam583@yahoo.in>
2024-04-24 22:05:27 +0530

    update

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className='flex justify-center items-center min-h-screen text-base md:text-4xl text-teal-500'>Welcome To Nextjs Development</div>
   )
 }
 
