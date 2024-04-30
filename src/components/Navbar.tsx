67b479d3ca23b7010592f6c888dacf5322f9db11
Shvam Singh <singhshivam583@yahoo.in>
2024-04-30 10:43:47 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
 
     const user: User = session?.user as User
 
-
   return (
     <nav className='p-2 md:p-3 shadow-md'>
         <div className='container mx-auto flex flex-col gap-2 md:flex-row justify-between items-center'>
